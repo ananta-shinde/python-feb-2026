848274a98d1cb9b7d694c5909112f69e384821af
aShinde <ananta.shinde@honeywellcom>
2026-04-10 08:55:01 +0530

    10/04/2026

--- OOPS/enacapsulation.py
+++ OOPS/enacapsulation.py
@@ -1,35 +1,31 @@
+from pandas import Period
 
-class Address:
-    atPost =None
-    city = None
-    state = None
-    country =None
-    pincode = None
-class College:
-    id = None
-    name = None
-    email = None
-    contact = None
-    address = Address()
 
-class Departmant:
-    id = None
-    name = None
-class Teacher:
-    empId = None
-    name = None
-    email = None
-    salary = None
-    contact = None
-    address = Address()
-    department = Departmant()
+class Person:
 
-class Student:
-    prn = None
-    name = None
-    email = None
-    contact = None
-    address = Address()
-    department = Departmant()
+    def __init__(self,prn):
+        self.__prn = prn
+        self.__name = None
+        self.email = None
+        self.password = None
 
+    def getName(self):
+        return self.__name
+
+    def setName(self,name):
+        if(isinstance(name,(str))):
+            self.__name = name
+
+    def getPrn(self):
+        return self.__prn
+
+
+p1 = Person(221212212)
+p1.email = "54545545454"
+p1.setName("Ananta")
+
+p2 = Person(5454545)
+print(p2.getPrn())
+
+print(p1.getPrn())
 
