4123433e6b3a60a89817207917108d953154549c
aShinde <ananta.shinde@honeywellcom>
2026-05-06 09:13:07 +0530

    06/05/2026

--- databaseconnectivity/demo01.py
+++ databaseconnectivity/demo01.py
@@ -7,14 +7,43 @@ user = "root"
 password = "Demo@123"
 schema = "sbi_db"
 
-try:
-    connection = db.connect(host=url,user =user,password=password,database=schema)
-    if(connection.is_connected()):
-        print("db connected successfully")
-    cursor = connection.cursor()
-    cursor.execute("insert into bank (bank_id,name,contact,address) values (100,'SBI',457845445,'Pune')")
-    connection.commit()
-except mysql.connector.Error as err:
-    print(err)
+
+def insertData(id,name,contact,address):
+        try:
+            connection = db.connect(host=url, user=user, password=password, database=schema)
+            if (connection.is_connected()):
+                print("db connected successfully")
+            cursor = connection.cursor()
+            cursor.execute("insert into bank (bank_id,name,contact,address) values ("+str(id)+",'"+name+"','"+contact+"','"+address+"')")
+            connection.commit()
+        except mysql.connector.Error as err:
+            print(err)
+
+
+def updateData():
+        try:
+            connection = db.connect(host=url, user=user, password=password, database=schema)
+            if (connection.is_connected()):
+                print("db connected successfully")
+            cursor = connection.cursor()
+            cursor.execute("update bank set contact='151212323' where bank_id=101")
+            connection.commit()
+        except mysql.connector.Error as err:
+            print(err)
+
+
+def deleteData():
+    try:
+        connection = db.connect(host=url, user=user, password=password, database=schema)
+        if (connection.is_connected()):
+            print("db connected successfully")
+        cursor = connection.cursor()
+        cursor.execute("delete from bank where bank_id=100")
+        connection.commit()
+    except mysql.connector.Error as err:
+        print(err)
+
+
+insertData(102,"SBI","56124545","Mumbai")
 
 
